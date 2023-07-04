7356ad4ff5e2d308ad70060eaa1dfaf6ad36a290
Jack Cai <Jack_Cai@wistron.com>
2023-07-04 13:53:55 +0800

    push code

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import Parse from 'parse/dist/parse.min.js';
+
+const appId = process.env.REACT_APP_PARSE_APP_ID;
+const hostUrl =  process.env.REACT_APP_PARSE_HOST_URL;
+const javascriptKey = process.env.REACT_APP_PARSE_JAVASCRIPT_KEY;
+
+Parse.initialize(appId, javascriptKey);
+Parse.serverURL = hostUrl;
+
+function App() {
+
+  const fetdata = async () =>{
+    const query = new Parse.Query('test')
+    const allData = await query.find();
+    allData.forEach(item => {
+      console.log(item)  
+    });
+  }
+
+  fetdata();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
